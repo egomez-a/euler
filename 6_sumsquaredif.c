d290c8e20c648199c9e24cdd65b21ee3d8e27830
Eugenio Gomez-acebo <egomez-a@c1r17s5.42madrid.com>
2023-02-14 15:40:14 +0100

    6 resuelto

--- 6_sumsquaredif.c
+++ 6_sumsquaredif.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   4_largestpalindrome.c                              :+:      :+:    :+:   */
+/*   6_sumsquaredif.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: egomez-a <egomez-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 12:10:14 by egomez-a          #+#    #+#             */
-/*   Updated: 2023/02/14 15:32:25 by egomez-a         ###   ########.fr       */
+/*   Updated: 2023/02/14 15:39:44 by egomez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,33 @@
 
 int main(void)
 {
-	
+	int n;
+    int i;
+    int squares;
+    int sumsquares;
+    int sum;
+    int squaresum;
+
+    i = 1;
+    n = 100;
+    sumsquares = 0;
+    squaresum = 0;
+    squares = 0;
+    sum = 0;
+    while (i <= n)
+    {
+        squares = i * i;
+        sumsquares = squares + sumsquares;
+        i++;
+    }
+    printf("sum of the squares of %d numbers is %d\n", n, sumsquares);
+    i = 1;
+    while (i <= n)
+    {
+        sum = sum + i;
+        i++;
+    }
+    squaresum = sum * sum; 
+    printf("square of sum of %d numbers is %d\n", n, squaresum);
+    printf("Difference is %d", squaresum - sumsquares);   
 }
